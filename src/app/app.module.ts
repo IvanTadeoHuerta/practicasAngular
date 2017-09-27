a93a0303fd4cb707fe93da38d26979aa1f209940
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-09-27 00:42:48 -0500

    Configuracion de firebase en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,19 @@ import { EspecificoComponent } from './components/especifico/especifico.componen
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { LugaresService } from './services/lugares.service';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCDjpZcx-TYR56Qy_VSf1efdRghdra5KQM",
+  authDomain: "platzisquare-9a618.firebaseapp.com",
+  databaseURL: "https://platzisquare-9a618.firebaseio.com",
+  storageBucket: "platzisquare-9a618.appspot.com",
+  messagingSenderId: "554311793407"
+};
+
+
 const appRoutes: Routes = [
   {path:'practicas', component: PracticasComponent},
   {path:'mapa', component: MapaComponent},
@@ -43,7 +56,10 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes, {useHash: true}),
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBUcY2wKFpZO9J_t9UCrnL_f1kmDeNtosY'
-    })
+    }),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [LugaresService],
   bootstrap: [AppComponent]
